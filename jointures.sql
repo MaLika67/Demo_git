98d022dfdbe0e3640b76f1a5886fc18d06918688
MaLika67 <135116400+MaLika67@users.noreply.github.com>
2024-11-01 18:33:21 +0100

    code de jointure ajouté

--- jointures.sql
+++ jointures.sql
@@ -4,4 +4,27 @@ select col1, col2, col3
 from table1
 join table2
 on table1.id = table2.id
--- Ma premiere jointure
+-- 
+
+-- Left join
+
+select *
+from table1
+from table1
+left join table2
+on table1.id= table2.id;
+
+-- Right join
+
+select * 
+from table1
+Right join table2
+on table1.id= table2.id;
+
+
+-- Inner join
+
+select * 
+from table1
+Inner join table2
+on table1.id= table2.id;
