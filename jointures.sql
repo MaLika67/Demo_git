57a48b4568e5bd82b7153da3637ef06099e7f81b
MaLika67 <135116400+MaLika67@users.noreply.github.com>
2024-11-01 18:37:52 +0100

    modification de code de jointures

--- jointures.sql
+++ jointures.sql
@@ -3,15 +3,14 @@
 select col1, col2, col3
 from table1
 join table2
-on table1.id = table2.id
--- 
+on table1.id = table2.id;
+
 
 -- Left join
 
 select *
 from table1
-from table1
-left join table2
+join table2
 on table1.id= table2.id;
 
 -- Right join
